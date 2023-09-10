f5c7158e852a64ff7d256aaad9a3b00720779093
Егор Есенский <egor@MacBook-Pro--Egor.local>
2023-09-10 22:30:14 +0300

    micro updates in StartAnimaionPage

--- app/StartAnimationPage/page.tsx
+++ app/StartAnimationPage/page.tsx
@@ -1,5 +1,7 @@
+'use client'
+
 import React from 'react';
-import styles from './page.module.css';
+import styles from "./page.module.css";
 
 const StartAnimationPage = () => {
   return (
@@ -8,7 +10,7 @@ const StartAnimationPage = () => {
         <img  src="./MAIN_ENVI.svg" alt="SVG Image" width="400" height="400"  />
       </div>
 
-      <button className='wow' style={{ marginTop: '10px' }}>Click Me</button>
+      <button className={styles.wow}>Click Me</button>
     </div>
   );
 };
