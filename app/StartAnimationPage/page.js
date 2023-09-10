010e018300546d9af8f7d4eb509bc625c33e05b5
Егор Есенский <egor@MacBook-Pro--Egor.local>
2023-09-10 22:19:37 +0300

    create StartAnimationPage

---
+++ app/StartAnimationPage/page.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import styles from './page.module.css';
+
+const StartAnimationPage = () => {
+  return (
+    <div style={{ backgroundColor: 'white', height: '100vh', display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', margin: '0' }}>
+      <div style={{}}>
+        <img  src="./MAIN_ENVI.svg" alt="SVG Image" width="400" height="400"  />
+      </div>
+
+      <button className='wow' style={{ marginTop: '10px' }}>Click Me</button>
+    </div>
+  );
+};
+
+export default StartAnimationPage;
