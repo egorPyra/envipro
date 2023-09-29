e530e213d470126fd24c573528d127d5a883cf7b
Matvey Glushko <matveyka_glushko@mail.ru>
2023-09-29 16:55:06 +0300

    images

--- app/components/MainPageBlocks/MainPageBlocks.tsx
+++ app/components/MainPageBlocks/MainPageBlocks.tsx
@@ -4,27 +4,32 @@ export default function MainPageBlocks() {
   return (
     <div className={styles.blocksContainer}>
       <div className={styles.sectionOne}>
-        <div className={styles.ecoRandom}>eco</div>
-        <div className={styles.ecology}>ecology</div>
+        <div className={styles.ecoRandom}></div>
+        <div className={styles.ecology}></div>
       </div>
 
       <div className={styles.sectionTwo}>
         <div className={styles.sectionTwo_subBlock}>
-          <div className={styles.earth}>earth</div>
-          <div className={styles.ecoShadow}>eco(shadow)</div>
+          <div className={styles.earth}></div>
+          <div className={styles.ecoShadow}></div>
         </div>
-        <div className={styles.engineer}>engineer costs</div>
-        <div className={styles.saveNature}>save nature</div>
+        <div>
+          <div className={styles.sectionTwo__engineerSaveNature}>
+            <div className={styles.engineer}></div>
+            <div className={styles.saveNature}>СОХРАНЯЯ ПРИРОДУ ДЛЯ БУДУЩИХ ПОКОЛЕНИЙ</div>
+          </div>
+          <div className={styles.ppa}></div>
+        </div>
       </div>
 
       <div className={styles.sectionThree}>
         <div className={styles.catPlasmaContainer}>
-          <div className={styles.cat}>cat track</div>
-          <div className={styles.plasma}>plasma</div>
+          <div className={styles.cat}></div>
+          <div className={styles.plasma}></div>
         </div>
         <div className={styles.calcFormulaContainer}>
-          <div className={styles.calc}>calculations</div>
-          <div className={styles.formula}>formula</div>
+          <div className={styles.calc}></div>
+          <div className={styles.formula}></div>
         </div>
       </div>
     </div>
