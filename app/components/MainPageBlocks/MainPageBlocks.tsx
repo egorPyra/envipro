aac24d18d848d20dab810396ae25e2db537b6f93
Matvey Glushko <matveyka_glushko@mail.ru>
2023-10-24 02:15:14 +0300

    hover prototype

--- app/components/MainPageBlocks/MainPageBlocks.tsx
+++ app/components/MainPageBlocks/MainPageBlocks.tsx
@@ -4,32 +4,61 @@ export default function MainPageBlocks() {
   return (
     <div className={styles.blocksContainer}>
       <div className={styles.sectionOne}>
-        <div className={styles.ecoRandom}></div>
-        <div className={styles.ecology}></div>
+        <div className={styles.ecoRandom}>
+          <span>ЭКОЛОГИЧЕСКИЙ<br/> МОНИТОРИНГ</span>
+        </div>
+        <div className={styles.ecology}>
+          <span>ЭКОЛОГИЧЕСКОЕ<br/> СОПРОВОЖДЕНИЕ<br/> ДЕЯТЕЛЬНОСТИ</span>
+        </div>
       </div>
 
       <div className={styles.sectionTwo}>
         <div className={styles.sectionTwo_subBlock}>
-          <div className={styles.earth}></div>
-          <div className={styles.ecoShadow}></div>
-        </div>
-        <div>
-          <div className={styles.sectionTwo__engineerSaveNature}>
-            <div className={styles.engineer}></div>
-            <div className={styles.saveNature}>СОХРАНЯЯ ПРИРОДУ ДЛЯ БУДУЩИХ ПОКОЛЕНИЙ</div>
+          <div className={styles.earth}>
+            <span>НЕДРО<br/> ПОЛЬЗО<br/> ВАНИЕ</span>
           </div>
-          <div className={styles.ppa}></div>
+          <div className={styles.ecoShadow}>
+            <span>ПРОИЗВОДСТВЕННЫЙ<br/> ЭКОЛОГИЧЕСКИЙ<br/> КОНТРОЛЬ</span>
+          </div>
+        </div>
+        <div className={styles.sectionTwo_ppaEngineer}>
+          <div className={styles.sectionTwo__engineerSaveNature}>
+            <div className={styles.engineer}>
+              <span>ИНЖЕНЕРНЫЕ<br/> ИЗЫСКАНИЯ</span>
+            </div>
+            <div className={styles.saveNature}>СОХРАНЯЯ ПРИРОДУ<br/> ДЛЯ БУДУЩИХ ПОКОЛЕНИЙ</div>
+          </div>
+          <div className={styles.ppa}>
+            <div className={styles.ppaAbove}/>
+            <div className={styles.ppaUnder}>
+              <span>РАЗРАБОТКА<br/> РАЗРЕШИТЕЛЬНОЙ<br/> ПРИРОДООХРАННОЙ<br/> ДОКУМЕНТАЦИИ</span>
+            </div>
+          </div>
         </div>
       </div>
 
       <div className={styles.sectionThree}>
         <div className={styles.catPlasmaContainer}>
-          <div className={styles.cat}></div>
-          <div className={styles.plasma}></div>
+          <div className={styles.cat}>
+            <div className={styles.catAbove}/>
+            <div className={styles.catUnder}>
+              <span>КРАСНАЯ<br/> КНИГА</span>
+            </div>
+          </div>
+          <div className={styles.plasma}>
+            <span>ГЕОДЕЗИЯ И<br/> КАРТОГРАФИЯ</span>
+          </div>
         </div>
         <div className={styles.calcFormulaContainer}>
-          <div className={styles.calc}></div>
-          <div className={styles.formula}></div>
+          <div className={styles.calc}>
+            <span>ПРОЕКТИРОВАНИЕ</span>
+          </div>
+          <div>
+            <div className={styles.forest}>
+              <span>ЛЕСОПОЛЬЗОВАНИЕ</span>
+            </div>
+            <div className={styles.formula}></div>
+          </div>
         </div>
       </div>
     </div>
