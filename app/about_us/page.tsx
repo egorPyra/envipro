db9f7099f7d39c4616b2c8f539c9a2e0080db54c
Matvey Glushko <matveyka_glushko@mail.ru>
2023-12-09 12:59:32 +0300

    save animation testing

--- app/about_us/page.tsx
+++ app/about_us/page.tsx
@@ -6,8 +6,23 @@ import MapSection from "../components/MapSection/MapSection";
 import KeepImprove from "../components/KeepImprove/KeepImprove";
 import HistorySection from "./components/HistorySection/HistorySection";
 import TeamSection from "./components/TeamSection/TeamSection";
+import { useEffect, useState } from "react";
+import { useMotionValueEvent, useScroll } from "framer-motion";
 
 export default function AboutUs() {
+
+  const { scrollY } = useScroll();
+
+  useMotionValueEvent(scrollY, "change", (latest) => {
+    console.log(latest)
+  })
+
+  // useEffect(() => {
+  //   console.log('heool')
+  //   const unsub = scrollY.on("change", (latest) => console.log(latest));
+  //   return () => unsub();
+  // }, [scrollY])
+
   return (
     <>
       <main className={styles.main}>
