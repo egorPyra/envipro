79c0a314cd90acf0d8eca3419a720bb72dd2e9a3
Matvey Glushko <matveyka_glushko@mail.ru>
2023-12-20 12:57:14 +0300

    routing fix 2.0

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,24 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
+
+const root = resolve(__dirname)
+const outDir = resolve(__dirname, 'dist')
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  root,
   plugins: [react()],
+  build: {
+    outDir,
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: resolve(root, 'index.html'),
+        about: resolve(root, 'about_us', 'index.html'),
+        contacts: resolve(root, 'contacts', 'index.html'),
+        secvices: resolve(root, 'secvices', 'index.html'),
+      }
+    }
+  }
 })
