dbfac1d07056ad079de54b92c5f94dff3a6d3cff
Matvey Glushko <matveyka_glushko@mail.ru>
2023-12-21 19:17:23 +0300

    back to next.js prod

--- app/components/Block/Block.tsx
+++ app/components/Block/Block.tsx
@@ -1,6 +1,5 @@
 import styles from './mainPageBlocks.module.css'
 import {useRef, useState} from "react";
-import { CSSTransition } from 'react-transition-group';
 
 export default function Block() {
 
