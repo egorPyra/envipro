eadc44169a328db172ff20d9e1c4b20e29684640
Matvey Glushko <matveyka_glushko@mail.ru>
2023-12-29 16:48:55 +0300

    nothing

--- app/page.tsx
+++ app/page.tsx
@@ -5,13 +5,14 @@ import MainPageBlocks from "@/app/components/MainPageBlocks/MainPageBlocks";
 import styles from "./page.module.css";
 import BurgerBtn from "./components/BurgerMenu/BurgerBtn";
 import BurgerContent from "./components/BurgerMenu/BurgerContent";
+import LogoPreloader from "./components/LogoPreloader/LogoPreloader";
 
 export default function Home() {
   const [isMenuActive, setIsMenuActive] = useState(false);
 
   return (
     <section className={styles.content} onClick={() => setIsMenuActive(false)}>
-      {/*<LogoPreloader/>*/}
+      {/* <LogoPreloader/> */}
       <BurgerBtn activateMenuFunc={setIsMenuActive}/>
       <BurgerContent isActive={isMenuActive} activateMenuFunc={setIsMenuActive}/>
       <MainPageBlocks/>
