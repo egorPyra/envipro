50856de18d112ab1f01c73e5bcce156a9b6d9fd2
Egor Esensky <egortus@gmail.com>
2025-05-08 13:33:49 +0300

    mini fixes 2

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,7 +13,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
 
   return (
-    <html lang="ru">
+    <html lang="ru" suppressHydrationWarning>
       <body className={inter.className}>
         {children}
       </body>
