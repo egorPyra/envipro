104c957b4d296492824d7a25ad0c4e3de005d187
egorPyra <egortus@gmail.com>
2026-03-14 15:04:36 +0800

    -inter

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,10 @@
 import './globals.css'
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
 import React from "react";
-
-const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: {
-    default: 'ЭнвиПро — экологическая организаия',
+    default: 'ЭнвиПро — экологическая организация',
     template: '%s | ЭнвиПро'
   },
 
@@ -21,13 +18,12 @@ export const metadata: Metadata = {
     'экологическая компания',
     'экологические услуги'
   ],
-
 }
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="ru">
-      <body className={inter.className}>
+      <body>
         {children}
       </body>
     </html>
